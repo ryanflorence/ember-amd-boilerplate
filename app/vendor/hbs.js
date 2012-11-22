9471c4d605dc0094710c8564dcd97e6974888029
Ryan Florence <rpflorence@gmail.com>
2012-11-21 22:26:59 -0700

    removed console.log

--- app/vendor/hbs.js
+++ app/vendor/hbs.js
@@ -1,6 +1,5 @@
 define({
   load: function(name, req, load, config) {
-    console.log('text!templates/' + name);
     req(['Ember', 'text!templates/' + name + '.handlebars'], function(Ember, text) {
       load(Ember.Handlebars.template(Ember.Handlebars.precompile(text)));
     });
